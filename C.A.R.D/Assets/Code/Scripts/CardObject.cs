8973d0dbcea3365b36f668a03609551d9b4fc551
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2018-05-24 17:45:00 +0100

    Added dynamic card images

---
+++ C.A.R.D/Assets/Code/Scripts/CardObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardObject : MonoBehaviour {
+
+    public const int IMAGE_INDEX = 0;
+    public const int CARDBACK_INDEX = 1;
+
+	void Start () {
+        Material imageMaterial = new Material(Shader.Find("Standard"));
+        imageMaterial.mainTexture = (Texture2D)Resources.Load("CardImages/AngryCat");
+        imageMaterial.name = "ImageMat";
+        GetComponent<MeshRenderer>().material = imageMaterial;
+	}
+}
