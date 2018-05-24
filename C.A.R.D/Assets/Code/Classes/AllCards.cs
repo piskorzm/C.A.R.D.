ae4c2fb91c430b06d6104ac9fb4f74df53d7a6b3
Marcin Piskorz <marcinpiskorz01@gmail.com>
2018-05-24 16:24:50 +0200

    More minions added.

--- C.A.R.D/Assets/Code/Classes/AllCards.cs
+++ C.A.R.D/Assets/Code/Classes/AllCards.cs
@@ -3,8 +3,11 @@
     //Spels
     public static string SmallRock = "Small Rock";
     public static string BigRock = "Big Rock";
+    public static string MoteRunner = "Mote Runner";
 
     //Minions
     public static string SpikeyMikey = "Spikey Mikey";
     public static string Fano = "Fano";
+    public static string DrPlump = "Dr.Plump";
+    public static string AngryGoose = "Angry Goose";
 }
--- C.A.R.D/Assets/Code/Classes/CardDictionary.cs
+++ C.A.R.D/Assets/Code/Classes/CardDictionary.cs
@@ -26,13 +26,24 @@ public class CardDictionary {
         SpellCard smallRock = new SpellCard(AllCards.SmallRock, "It's a small rock!", 0, new Effect(TargetType.TARGET, 1));
         _allCards.Add(AllCards.SmallRock, smallRock);
 
-        SpellCard bigRock = new SpellCard(AllCards.BigRock, "It's a BIG rock!", 0, new Effect(TargetType.TARGET, 3));
+        SpellCard bigRock = new SpellCard(AllCards.BigRock, "It's a BIG rock!", 2, new Effect(TargetType.TARGET, 3));
         _allCards.Add(AllCards.BigRock, bigRock);
 
-        MinionCard spikeyMikey = new MinionCard(AllCards.SpikeyMikey, "Watch out, he is spikey.", 4, 11, 1);
-        _allCards.Add(AllCards.SpikeyMikey, bigRock);
+        SpellCard moteRunner = new SpellCard(AllCards.MoteRunner, "Gives cancer to your opponent", 10, new Effect(TargetType.TARGET, 100));
+        _allCards.Add(AllCards.BigRock, moteRunner);
 
-        MinionCard fano = new MinionCard(AllCards.Fano, "Fano is just not very good.", 10, 0, 1);
+
+        MinionCard spikeyMikey = new MinionCard(AllCards.SpikeyMikey, "Watch out, he is spikey", 5, 11, 1);
+        _allCards.Add(AllCards.SpikeyMikey, spikeyMikey);
+
+        MinionCard fano = new MinionCard(AllCards.Fano, "Fano is just not very good", 10, 0, 1);
         _allCards.Add(AllCards.Fano, fano);
+
+        //TODO add "HAHAHA, it's so easy" on play
+        MinionCard drPlump = new MinionCard(AllCards.DrPlump, "Destroys opponets moral status", 4, 6, 4);
+        _allCards.Add(AllCards.DrPlump, drPlump);
+
+        MinionCard angryGoose = new MinionCard(AllCards.AngryGoose, "Beter start running now!", 3, 4, 3);
+        _allCards.Add(AllCards.AngryGoose, angryGoose);
     }
 }
