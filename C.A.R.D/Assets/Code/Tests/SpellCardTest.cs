e2d652fa9aebf5ec9d1cbf0c2b127b3816d5d684
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2018-06-29 21:23:14 +0100

    Added testing

---
+++ C.A.R.D/Assets/Code/Tests/SpellCardTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class SpellCardTest {
+
+    [Test]
+	public void CreateSpellCardTest
+	{
+		
+	}
+}
