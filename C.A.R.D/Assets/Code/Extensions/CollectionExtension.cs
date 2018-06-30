c02306ae238d633906648edb08c6fe43563614ae
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2018-06-30 23:36:02 +0100

    Added collection extension methods

---
+++ C.A.R.D/Assets/Code/Extensions/CollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class CollectionExtension
+{
+	public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+	{
+		return source.OrderBy(x => Guid.NewGuid());
+	}
+
+	public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count)
+	{
+		return source.Shuffle().Take(count);
+	}
+
+	public static T PickRandom<T>(this IEnumerable<T> source)
+	{
+		return source.PickRandom(1).Single();
+	}
+}
--- C.A.R.D/Assets/Code/Scripts/CardManager.cs
+++ C.A.R.D/Assets/Code/Scripts/CardManager.cs
@@ -39,6 +39,9 @@ public class CardManager : MonoBehaviour
     /// </summary>
     private void LoadAllCards()
 	{
+		//Create an empty dictionary to populate with cards
+		_allCards = new Dictionary<int, Card>();
+
 		//Read card JSON data and generate a list of cards from it
 		TextAsset cardDataRaw = Resources.Load<TextAsset>("Cards");
 		List<Card> generatedCards = JSONReader.GenerateCardsFromJSON(cardDataRaw.text);		
@@ -48,5 +51,22 @@ public class CardManager : MonoBehaviour
 		{
 			_allCards.Add(c.Id, c);
 		}
+	}
+
+	public Card GetCardWithID(int id)
+	{
+		//Throw an exception if a card with the specified ID does not exist
+		if(!_allCards.ContainsKey(id))
+		{
+			throw new KeyNotFoundException("Can't find card with ID: " + id);
+		}
+
+		//Return the desired card
+		return _allCards[id];
+	}
+
+	public Card GetRandomCard()
+	{
+		return _allCards[CardIds.PickRandom()];
 	}
 }
