b4de69f009418394c8a62d8fdd58299d58586bdd
Marcin Piskorz <marcinpiskorz01@gmail.com>
2018-06-30 16:19:30 +0200

    JsonReaderTest fix

--- C.A.R.D/Assets/Tests/JSONReaderTest.cs
+++ C.A.R.D/Assets/Tests/JSONReaderTest.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 
 public class JSONReaderTest {
 
-	private const string OneSpellAndMinionCardJSON = "{\"spellCards\": [{\"id\": 0,\"name\": \"Small Rock\",\"rarity\": \"Common\",\"description\": \"It's a small rock\",\"cost\": 0,\"path\": \"SmallRock\",\"effects\": [	{\"targetType\": \"Target\",\"damage\": 1}]}]," +
-        "\"minionCards\":[{\"id\": 1,\"name\": \"Spikey Mikey\",\"rarity\": \"Rare\",\"description\": \"Watch out, he is spikey\",\"cost\": 5,\"path\": \"SpikeyMikey\",\"attack\": 11,\"health\": 1]}";
+	private const string OneSpellCardJSON = "{\"spellCards\": [{\"id\": 0,\"name\": \"Small Rock\",\"rarity\": \"Common\",\"description\": \"It's a small rock\",\"cost\": 0,\"path\": \"SmallRock\",\"effects\": [	{\"targetType\": \"Target\",\"damage\": 1}]}],\"minionCards\":[]}";
+	private const string OneMinionCardJSON = "{\"spellCards\": [],\"minionCards\":[{\"id\": 1,\"name\": \"Spikey Mikey\",\"rarity\": \"Rare\",\"description\": \"Watch out, he is spikey\",\"cost\": 5,\"path\": \"SpikeyMikey\",\"attack\": 11,\"health\": 1}]}";
 
 	[Test]
 	public void ReadOneSpellCardJSON()
 	{
 		//Read the one spell card json data and generate a list of cards from it, which should contain only one card
-		List<Card> cards = JSONReader.GenerateCardsFromJSON(OneSpellAndMinionCardJSON);
+		List<Card> cards = JSONReader.GenerateCardsFromJSON(OneSpellCardJSON);
 
 		//Ensure the length of this card list is two
-		Assert.AreEqual(2, cards.Count);
+		Assert.AreEqual(1, cards.Count);
 
 		//Ensure the cards were generated correctly
 		Assert.AreEqual("Small Rock", cards[0].Name);
-		Assert.AreEqual("Spikey Mikey", cards[1].Name);
 	}
+
+    [Test]
+    public void ReadOneMinionCardJSON()
+    {
+        //Read the one spell card json data and generate a list of cards from it, which should contain only one card
+        List<Card> cards = JSONReader.GenerateCardsFromJSON(OneMinionCardJSON);
+
+        //Ensure the length of this card list is two
+        Assert.AreEqual(1, cards.Count);
+
+        //Ensure the cards were generated correctly
+        Assert.AreEqual("Spikey Mikey", cards[0].Name);
+    }
 }
